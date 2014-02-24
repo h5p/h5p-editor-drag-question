dd593fe6cc39c787bcfadafbefb551fb52845277
Frode Petterson <frode.petterson@amendor.com>
2014-02-24 16:26:07 +0100

    HFJ-179: Prepared for new API version. Using new resize events, removed some deprecated stuff. Fixed a bug in CP editor.

--- H5PEditor.DragQuestion.js
+++ H5PEditor.DragQuestion.js
@@ -1,43 +1,9 @@
 var H5PEditor = H5PEditor || {};
 
-var H5P = H5P || {};
-if (H5P.getPath === undefined) {
-  /**
-   * Find the path to the content files based on the id of the content
-   *
-   * Also identifies and returns absolute paths
-   *
-   * @param {String} path Absolute path to a file, or relative path to a file in the content folder
-   * @param {Number} contentId Identifier of the content requesting the path
-   * @returns {String} The path to use.
-   */
-  H5P.getPath = function (path, contentId) {
-    if (path.substr(0, 7) === 'http://' || path.substr(0, 8) === 'https://') {
-      return path;
-    }
-
-    return H5PIntegration.getContentPath(contentId) + path;
-  };
-}
-if (H5P.newInstance === undefined) {
-  /**
-   * Helps create new instance of H5P library.
-   *
-   * @param {Object} library Container library (the über name of the library(namespace, name and versionnumber)) and .
-   * @returns {Object} Instance of library
-   */
-  H5P.newInstance = function (library) {
-    // TODO: Add some try catching?
-    // TODO: Dynamically try to load libraries currently not loaded?
-    return new (H5P.classFromName(library.library.split(' ')[0]))(library.params, H5PEditor.contentId);
-  };
-}
-
-// TODO: Rewrite to use H5P.DragQuestion for previewing!
-
 /**
  * Interactive Video editor widget module
- *
+ * TODO: Rewrite to use H5P.DragQuestion for previewing?
+ 
  * @param {jQuery} $
  */
 H5PEditor.widgets.dragQuestion = H5PEditor.DragQuestion = (function ($) {
@@ -503,8 +469,7 @@ H5PEditor.widgets.dragQuestion = H5PEditor.DragQuestion = (function ($) {
     var params = this.params.elements[id];
 
     // Create and add new instance
-    element.instance = H5P.newInstance(params.type);
-    element.instance.attach(element.$element);
+    element.instance = H5P.newRunnable(params.type, H5PEditor.contentId, element.$element);
 
     // Make resize possible
     this.dnr.add(element.$element);
