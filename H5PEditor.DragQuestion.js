6a820bb94e6a723399382e64eedb3c2cf25d5bf0
Frode Petterson <frode.andre.petterson@gmail.com>
2015-06-24 16:41:29 +0200

    Center new elements.

--- H5PEditor.DragQuestion.js
+++ H5PEditor.DragQuestion.js
@@ -222,7 +222,6 @@ H5PEditor.widgets.dragQuestion = H5PEditor.DragQuestion = (function ($) {
         setTimeout(function () {
           that.dnb.dnd.$element.dblclick();
         }, 1);
-        that.dnb.newElement = false;
       }
     };
     this.dnb.attach(this.$dnbWrapper);
