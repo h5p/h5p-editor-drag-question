e11f9d273e82e65766f0db8fc43d2623f5c883ad
Thomas Marstrander <thomasmars87@gmail.com>
2015-09-10 09:48:51 +0200

    Make sure style is defined. [HFJ-999]

--- H5PEditor.DragQuestion.js
+++ H5PEditor.DragQuestion.js
@@ -934,6 +934,10 @@ H5PEditor.widgets.dragQuestion = H5PEditor.DragQuestion = (function ($) {
    * @param {Number} alpha
    */
   C.setAlphas = function (style, prefix, alpha) {
+    // Style undefined
+    if (!style) {
+      return;
+    }
     var colorStart = style.indexOf(prefix);
 
     while (colorStart !== -1) {
