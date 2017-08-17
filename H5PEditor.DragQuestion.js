1fe1d6950057add7d20d6d5cb9b19f6b8946593d
Andreas Nergaard <andreas.nergaard@cerpus.com>
2017-08-17 09:20:16 +0200

    HFP-221 Shortened and Improved code by removing needs of external options.

--- H5PEditor.DragQuestion.js
+++ H5PEditor.DragQuestion.js
@@ -562,19 +562,17 @@ H5PEditor.widgets.dragQuestion = H5PEditor.DragQuestion = (function ($, DragNBar
     }).appendTo(element.$element);
 
     setTimeout(function () {
-      var options = {};
       var type = (elementParams.type ? elementParams.type.library.split(' ')[0] : null);
 
-      if (type === 'H5P.Image') {
-        options.cornerLock = true;
-      }
-
-      var dnbElement = that.dnb.add(element.$element, DragNBar.clipboardify(clipboardKey, elementParams, 'type'), options);
+      var dnbElement = that.dnb.add(element.$element, DragNBar.clipboardify(clipboardKey, elementParams, 'type'), {
+        cornerLock: (type === 'H5P.Image')
+      });
 
       dnbElement.contextMenu.on('contextMenuEdit', function () {
         that.editElement(element);
         that.dnb.blurAll();
       });
+
       dnbElement.contextMenu.on('contextMenuRemove', that.elementRemove.bind(that, element));
       dnbElement.contextMenu.on('contextMenuBringToFront', that.elementBringToFront.bind(that, element));
       dnbElement.contextMenu.on('contextMenuSendToBack', that.elementSendToBack.bind(that, element));
